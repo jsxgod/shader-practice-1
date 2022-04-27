8bdea04b9f30d63087fd777e9e7f990d710442d4
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-27 22:07:29 +0200

    Add light. Add the custom shader material.

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, extend } from "@react-three/fiber";
+import CustomShaderMaterial from "../shaders/CustomShaderMaterial";
+
+extend({ CustomShaderMaterial });
 
 const Scene = () => {
   return (
     <Canvas>
       <mesh>
+        <pointLight position={[10, 10, 10]} />
         <planeBufferGeometry args={[3, 5]} />
+        <customShaderMaterial uColor="blue" />
       </mesh>
     </Canvas>
   );
