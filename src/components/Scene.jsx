dab0264a0d89d118db5c50dcd5623eeaa3306f57
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-27 23:27:37 +0200

    Render the Wave component

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Canvas, extend } from "@react-three/fiber";
 import CustomShaderMaterial from "../shaders/CustomShaderMaterial";
+import Wave from "./Wave";
 
 extend({ CustomShaderMaterial });
 
 const Scene = () => {
   return (
     <Canvas>
-      <mesh>
-        <pointLight position={[10, 10, 10]} />
-        <planeBufferGeometry args={[3, 5]} />
-        <customShaderMaterial uColor="blue" />
-      </mesh>
+      <Suspense fallback={null}>
+        <Wave />
+      </Suspense>
     </Canvas>
   );
 };
