60daff40d5000077b38c27c355e97dcb6ff3402e
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-28 00:50:06 +0200

    Load texture

--- src/components/Wave.jsx
+++ src/components/Wave.jsx
@@ -1,5 +1,7 @@
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import React, { useRef } from "react";
+import * as THREE from "three";
+import textureImage from "../art-01.jpg";
 
 const Wave = () => {
   const materialRef = useRef();
@@ -8,10 +10,18 @@ const Wave = () => {
     materialRef.current.uTime = clock.getElapsedTime();
   });
 
+  const imageTexture = useLoader(THREE.TextureLoader, textureImage);
+  const { degToRad } = THREE.MathUtils;
+
   return (
-    <mesh>
-      <planeBufferGeometry args={[0.4, 0.6, 16, 16]} />
-      <customShaderMaterial uColor="blue" ref={materialRef} wireframe />
+    <mesh rotation={[0.0, 0.0, degToRad(90)]}>
+      <planeBufferGeometry args={[1.0, 1.5, 16, 16]} />
+      <customShaderMaterial
+        uColor="blue"
+        ref={materialRef}
+        uTexture={imageTexture}
+        uSlowDownFactor={3.0}
+      />
     </mesh>
   );
 };
