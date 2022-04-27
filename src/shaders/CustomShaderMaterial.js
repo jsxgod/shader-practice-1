15a7c3959e7dc1e9f81be968d5c024c678adc30e
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-27 23:58:44 +0200

    Add noise to the vertex shader

--- src/shaders/CustomShaderMaterial.js
+++ src/shaders/CustomShaderMaterial.js
@@ -9,10 +9,20 @@ const CustomShaderMaterial = shaderMaterial(
   //Vertex Shader
   glsl`
     varying vec2 vUv;
+    precision mediump float;
+    uniform float uTime;
+
+    #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
 
     void main () {
       vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      vec3 pos = position;
+      float noiseFreq = 1.5;
+      float noiseAmp = 0.25;
+      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+      pos.z += snoise3(noisePos) * noiseAmp;
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
     }
   `,
 
